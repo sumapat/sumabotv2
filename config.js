db4fea58f657b1674af65b54bc8966679f73af42
Suma Patil <suma.patil88@gmail.com>
2016-08-28 15:39:09 -0700

    Changed node to node6

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const config = require('config');
 
 // App Secret can be retrieved from the App Dashboard
-const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
+const APP_SECRET = (process.env.APP_SECRET) ?
     process.env.MESSENGER_APP_SECRET :
     config.get('appSecret');
 
 // Arbitrary value used to validate a webhook
-const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+const VALIDATION_TOKEN = (process.env.VALIDATION_TOKEN) ?
     (process.env.MESSENGER_VALIDATION_TOKEN) :
     config.get('validationToken');
 
 // Generate a page access token for your page from the App Dashboard
-const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+const PAGE_ACCESS_TOKEN = (process.env.PAGE_ACCESS_TOKEN) ?
     (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
     config.get('pageAccessToken');
 
